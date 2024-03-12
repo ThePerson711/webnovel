a47b2f4f3b12192f9110f63150c9bbb02f3f107f
ThePerson711 <147810054+ThePerson711@users.noreply.github.com>
2024-03-12 14:57:05 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+//
+let chap_1 = 
+`
+-Blaaargh!
+
+As soon as I regained my senses, I felt my stomach churning and before I knew it I was on the ground barfing.
+
+Fortunately, I didn't puke on my mask as I slightly tilted it upwards to allow the barf to escape from my mouth.
+
+"Sit down for a bit"
+
+Actually, before that, was my expression that easy to read? It was fine if only Grandmaster Keiki could read my thoughts...but now another person, whom I just met, could easily read what I wanted to say-was my face that easy to read?`;
